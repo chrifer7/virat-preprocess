92271aa8d375b917bf4ca5cbdd29897d1efd06c1
chrifer7 <chrifer7@gmail.com>
2017-09-21 15:50:29 -0500

    First commin

---
+++ split_video.py
@@ -0,0 +1,24 @@
+import csv
+import glob
+import os
+import os.path
+from subprocess import call
+
+
+video_name = 'VIRAT_S_050203_09_001960_002083'
+annotations_path = '/media/grupoavatar/DATOS/VIRAT/VIRAT_Video_Dataset_Release_1.0/Sample_Dataset/annotations/'
+video_path = '/media/grupoavatar/DATOS/VIRAT/VIRAT_Video_Dataset_Release_1.0/Sample_Dataset/videos/'
+
+vfile = open(annotations_path+video_name+'.viratdata.events.txt', 'r')
+
+i = 0
+for line in vfile:
+    vdata = line.split(' ')
+    print ('Event: ', vdata[1])
+    call(["ffmpeg", "-i", video_path+video_name+'.mp4', video_path+video_name+'_frames'+'-%04d.jpg'])
+    i = i + 1
+
+
+
+
+
