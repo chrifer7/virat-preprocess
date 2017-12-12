3ecc787b7e289a699f6eadbb338ffd02e0907dc4
L. Christian Fernández <fernandezm.christian@pucp.pe>
2017-12-12 16:54:07 -0500

    Script de distribución en categorías completado

--- utils/categorize_videos.py
+++ utils/categorize_videos.py
@@ -1,9 +1,68 @@
+"""
+==========================
+Annotation Files
+==========================
+
+Annotations files include three types of annotations per clip
+1) Event file (selected events annotated)
+2) Mapping file (from event to object)
+3) Object file (all objects annotated)
+
+The detailed formats of the above three types of (linked) files are described below.
+
+
+1) Event file format
+
+Files are named as '%s.viratdata.events.txt' where %s is clip id.
+Each line in event file captures information about a bounding box of an event at the corresponding frame
+
+Event File Columns
+1: event ID        (unique identifier per event within a clip, same eid can exist on different clips)
+2: event type      (event type)
+3: duration        (event duration in frames)
+4: start frame     (start frame of the event)
+5: end frame       (end frame of the event)
+6: current frame   (current frame number)
+7: bbox lefttop x  (horizontal x coordinate of left top of bbox, origin is lefttop of the frame)
+8: bbox lefttop y  (vertical y coordinate of left top of bbox, origin is lefttop of the frame)
+9: bbox width      (horizontal width of the bbox)
+10: bbox height    (vertical height of the bbox)
+
+Event Type ID (for column 2 above)
+1: Person loading an Object to a Vehicle
+2: Person Unloading an Object from a Car/Vehicle
+3: Person Opening a Vehicle/Car Trunk
+4: Person Closing a Vehicle/Car Trunk
+5: Person getting into a Vehicle
+6: Person getting out of a Vehicle
+7: Person gesturing
+8: Person digging
+9: Person carrying an object
+10: Person running
+11: Person entering a facility
+12: Person exiting a facility
+"""
+
 import sys
 import os
 import shutil
 
 number_of_categories = 0
-array_of_categories = [0] * 15
+array_of_categories = [0] * 13 # Cantidad de tipos eventos (no considero el cero) --por mejorar
+array_of_name_categories = ["",
+                            "1: Person loading an Object to a Vehicle",
+                            "2: Person Unloading an Object from a Car/Vehicle",
+                            "3: Person Opening a Vehicle/Car Trunk",
+                            "4: Person Closing a Vehicle/Car Trunk",
+                            "5: Person getting into a Vehicle",
+                            "6: Person getting out of a Vehicle",
+                            "7: Person gesturing",
+                            "8: Person digging",
+                            "9: Person carrying an object",
+                            "10: Person running",
+                            "11: Person entering a facility",
+                            "12: Person exiting a facility"
+                            ]
 
 if __name__ == '__main__':
     if not len(sys.argv) == 3:
@@ -24,6 +83,24 @@ if __name__ == '__main__':
                     number_of_categories = number_of_categories + 1
                     print(dest_video_cat_dir)
                 array_of_categories[int(video_cat[1])] = array_of_categories[int(video_cat[1])] + 1
-                shutil.move(os.path.join(video_dir, dir_name, video_dir_name), dest_video_cat_dir)
 
-        print (array_of_categories)
+                source_path = os.path.join(video_dir, dir_name, video_dir_name)
+                dest_path = os.path.join(dest_video_cat_dir, video_dir_name)
+
+                print('source_path: ', source_path)
+                print('dest_path: ', dest_path)
+
+                if os.path.exists(dest_path):
+                    # si el directorio existe no me dejará mover _todo el directorio, debo hacerlo un archivo a la vez
+                    for video_frame in os.listdir(source_path):
+                        source_path_file = os.path.join(source_path, video_frame)
+                        dest_path_file = os.path.join(dest_path, video_frame)
+                        print('source_path_file: ', source_path_file)
+                        print('dest_path_file: ', dest_path_file)
+                        shutil.move(source_path_file, dest_path_file)
+                else:
+                    # c.c. lo muevo
+                    shutil.move(source_path, dest_path)
+
+        for i in range(1, (len(array_of_categories) - 1)):
+            print (array_of_name_categories[i]+': '+str(array_of_categories[i]))
